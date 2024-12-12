c980b5f6cccacdd24c66ba243ddb4236c2d32888
MatteoVentura <matteo.ventura18@gmail.com>
2024-12-12 15:21:03 +0100

    add : snack-1

--- snack-1/script.js
+++ snack-1/script.js
@@ -8,3 +8,25 @@ Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e pe
 Stampare a schermo la bici con peso minore.
 
 */
+
+// Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+
+const biciclette = [
+    { nome: "Bianchi Sprint", peso: 20.5 },
+    { nome: "Mountainbike", peso: 38.2 },
+    { nome: "KTM", peso: 27.9 },
+    { nome: "Pinarello", peso: 22.4 }
+];
+
+let biciclettaLeggera = biciclette[0];
+
+// Ciclo che trova la bici con peso minore
+
+for (let i = 1; i < biciclette.length; i++) {
+    if (biciclette[i].peso < biciclettaLeggera.peso) {
+        biciclettaLeggera = biciclette[i];
+    }
+}
+
+// Stampare a schermo la bici con il peso minore
+console.log("La bici con il peso minore è:", biciclettaLeggera.nome, "con un peso di", biciclettaLeggera.peso, "kg.");
